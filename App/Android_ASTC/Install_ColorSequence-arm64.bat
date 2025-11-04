9331ee5a706fd3528e050bcf7d0ebc3ba699ff71
charlixcx-png <0260116@up.edu.mx>
2025-11-04 10:27:03 -0600

    Initial commit

---
+++ App/Android_ASTC/Install_ColorSequence-arm64.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:87bd9d8a7314f3623896e5a3851335b50e6a1340c8994e44e363a440c566eddd
+size 2013
---
+++ App/Android_ASTC/Uninstall_ColorSequence-arm64.bat
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ddb6e8e69388e47ed663a2668597e2230acd17703709d9fed7ab07ebed8fa8f3
+size 964
